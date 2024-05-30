e4610d9b3342c2f647bfe84064c76f67f1eb25df
liviaTI <liviasantferreira@gmail.com>
2024-05-30 00:41:41 -0300

    First commit

---
+++ coinToss.cs
@@ -0,0 +1,7 @@
+﻿/*In this challenge, you will implement a conditional operator to simulate a "coin toss." 
+The resulting decision logic will display heads or tails. */
+
+
+
+Random coin = new Random();
+Console.WriteLine((coin.Next(0, 2) == 0) ? "heads" : "tails");
